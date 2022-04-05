8fbffdf7f6e8de60fcac06dcb2633af9c5ffbdd7
Sue Roberts <suerobertstx@yahoo.com>
2022-04-05 11:07:43 -0500

    Add form to enter name, dob, and music by user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-  $("#formone").submit(function(event){
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
     const nameInput = $("input#name").val();
-
     const dobInput = $("#born").val();
     const musicInput = $("input:radio[name=music]:checked").val();
-
-    $("#music").text(musicInput);
-    $("#name").text(nameInput);
+    
+    $(".name").text(nameInput);
     $("#dob").text(dobInput);
+    $("#music").text(musicInput);
 
     $("#story").show();
   });
 });
+
